8896b135c0bbdee09aaee338f5e21a03252bce5f
Phil <philwekullo@gmail.com>
2024-04-11 10:58:21 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+import re
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
@@ -12,6 +13,21 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author's name cannot be empty.")
+
+        existing_author = Author.query.filter(Author.name == name).first()
+        if existing_author:
+            raise ValueError("Author's name must be unique.")
+        return name
+        
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and not re.match(r'^\d{10}$', phone_number):
+            raise ValueError("Phone number must be exactly ten digits.")
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +44,31 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Title must contain one of the following phrases: 'Won't Believe', 'Secret', 'Top', 'Guess'")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Summary must be a maximum of 250 characters.")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'.")
+        return category
+
 
 
     def __repr__(self):
